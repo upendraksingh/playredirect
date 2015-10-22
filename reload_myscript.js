e4e82c95d0829b55b377fa7c2006f2c517eccf92
unknown <upendracool@gmail.com>
2015-10-22 15:22:12 +0530

    pushing changes for redireting to iOS app store

--- reload_myscript.js
+++ reload_myscript.js
@@ -1,4 +1,33 @@
 function myfunction()
 {
 	window.open("https://play.google.com/store/apps/details?id=indwin.c3.shareapp","_self");
+	var appstoreFail = "www.themeshapp.com";
+	if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false)
+    {
+        //If the app is not installed the script will wait for 2sec and redirect to web.
+        var loadedAt = +new Date;
+        setTimeout(
+                   function(){
+                       if (+new Date - loadedAt < 2000){
+                   window.location = appstoreFail;
+                       }
+                   }
+                   ,25);
+        //Try launching the app using URL schemes
+        window.open(https://itunes.apple.com/in/app/bigbasket.com/id660683603?mt=8,"_self");
+    } else if(navigator.userAgent.match(/(Android)/g) ? true : false){
+		var loadedAt = +new Date;
+        setTimeout(
+                   function(){
+                       if (+new Date - loadedAt < 2000){
+                   window.location = appstoreFail;
+                       }
+                   }
+                   ,25);
+        //Try launching the app using URL schemes
+        window.open("https://play.google.com/store/apps/details?id=indwin.c3.shareapp","_self");
+        window.location = weblink;
+    } else {
+		window.location = appstoreFail;
+	}
 }
