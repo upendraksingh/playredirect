cd624e64c2a8b625ebdd47ea829b9d2d69085228
unknown <upendracool@gmail.com>
2015-10-22 18:08:32 +0530

    minor changes

--- reload_myscript.js
+++ reload_myscript.js
@@ -1,29 +1,15 @@
 function myfunction()
 {
-	var appstoreFail = "http://themeshapp.com/";
-	if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false)
-    {
-        //If the app is not installed the script will wait for 2sec and redirect to web.
-        var loadedAt = +new Date;
-        setTimeout(
-                   function(){
-                       if (+new Date - loadedAt < 2000){
-                   window.location = appstoreFail;
-                       }
-                   }
-                   ,25);
-        window.open("https://itunes.apple.com/in/app/bigbasket.com/id660683603?mt=8","_self");
+	if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false){
+		
+		window.open("https://itunes.apple.com/in/app/bigbasket.com/id660683603?mt=8","_self");
 		
     } else if(navigator.userAgent.match(/(android)/ig) ? true : false){
-		var loadedAt = +new Date;
-        setTimeout(
-                   function(){
-                       if (+new Date - loadedAt < 2000){
-                   window.location = appstoreFail;
-                       }
-                   }
-                   ,25);
+		
         window.open("https://play.google.com/store/apps/details?id=indwin.c3.shareapp","_self");
 		
-    }
+    } else {
+		
+		window.open("http://themeshapp.com/","_self");
+	}
 }
