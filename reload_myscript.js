7c0dff04603a8ea0bd8e07b266b76bd19e270a31
unknown <upendracool@gmail.com>
2015-10-22 18:47:25 +0530

    minor changes

--- reload_myscript.js
+++ reload_myscript.js
@@ -1,8 +1,8 @@
 function myfunction(){
-    if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false){
+    if (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false) {
         window.open("https://itunes.apple.com/in/app/bigbasket.com/id660683603?mt=8","_self");
 
-    } else if(navigator.userAgent.match(/(android)/ig) ? true : false{
+    } else if(navigator.userAgent.match(/(android)/ig) ? true : false) {
         window.open("https://play.google.com/store/apps/details?id=indwin.c3.shareapp","_self");
 
     } else {
